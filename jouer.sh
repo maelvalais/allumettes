774414d713a0f2833bcd117a7c8a6cda4876843e
Maël Valais <mael.valais@gmail.com>
2017-06-05 14:35:44 +0200

    ajout couleurs

--- jouer.sh
+++ jouer.sh
@@ -1,27 +1,43 @@
 #! /usr/bin/env bash
 #set -ex
 
+DEBUG=
+for p in $@; do
+    case $p in
+        -d|--debug) DEBUG="yes";;
+    esac
+done
+
+# Parameter: turn number
+function joueur() {
+    if (($1 == 0)); then
+        echo -ne "\033[92mjoueur $1\033[0m"
+    else
+        echo -ne "\033[91mjoueur $1\033[0m"
+    fi
+}
+
 cp allumettes2.touist temp
 a_gagne=false
 while ! $a_gagne; do
     touist temp --solve --qbf | sort -k2 -t"(" | grep '^[^?] ' > result
-    if grep "0 0_a_perdu" result; then echo "Joueur 0 a gagné"; exit 0; fi
+    if grep "0_a_perdu" result; then echo "Joueur 0 a gagné"; exit 0; fi
     while read line; do
         nb_allum=$(echo $line | sed "s/^\([01]\) .*/\1/g")
         num_coup=$(echo $line | sed "s/^[01] prend2(\([0-9][0-9]*\))/\1/g")
         #perl -n -e '/([01]) prend2\((\d+)\)$/ && print $1 $2'
-        echo "Au coup $num_coup: le joueur $((num_coup % 2)) doit prendre $([ "$nb_allum" -eq 0 ] && echo 1 || echo 2) allumettes"
-    done < result
+        echo "Tour $num_coup: $(joueur $((num_coup%2))) doit prendre $([ "$nb_allum" -eq 0 ] && echo 1 || echo 2) allumettes"
+    done < <(tail -1 result)
 
     # Now, we want to replace the forall by a exists:
     #             forall prend2(7):
     # becomes     exists prend2(7): [not] prend2(7) and
 
-    echo "Adversaire prend 1 ou 2 au tour $((num_coup+1)) ?"
+    echo -n "Tour $((num_coup+1)): $(joueur $(((num_coup+1)%2)))  prend 1 ou 2 au tour $((num_coup+1)) ? "
     read choix
-    sed "s/^forall prend2($((num_coup+1))):/exists prend2($((num_coup+1))): $([[ $choix -eq 1 ]] && echo not) prend2($((num_coup+1))) and/g" temp > $$ && mv $$ temp
+    sed "s/^\(forall\|exists\) prend2($((num_coup+1))):.*/exists prend2($((num_coup+1))): $([[ $choix -eq 1 ]] && echo not) prend2($((num_coup+1))) and/g" temp > $$ && mv $$ temp
 
-    grep "\(exists\|forall\)" temp | while IFS= read line; do
+    [ -z $DEBUG ] || grep "\(exists\|forall\)" temp | while IFS= read line; do
         echo -e "\033[90m${line}\033[0m"
     done
 done
